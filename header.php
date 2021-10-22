e56704fcbe562dd34b90394f77918db9229e2024
dreamCode <np03a170120@heraldcollege.edu.np>
2021-10-22 16:45:51 +0545

    IC

---
+++ header.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;700;900&display=swap" rel="stylesheet"> -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Roboto:wght@100;400;700;900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./fontawesome-free-5.15.2-web/css/all.css">
+    <link rel="stylesheet" href="./css/main.css">
+    <title>Baliyo Construction</title>
+</head>
+
+<body>
+    <!-- HEADER -->
+    <header class="main-header">
+        <div class="header-top j-s-c">
+            <ul class="location_email j-c">
+                <li><i class="fas fa-phone-alt"></i><span>Maitidevi, Kathmandu</span></li>
+                <li><i class="fas fa-envelope"></i><span>company@email.com</span></li>
+            </ul>
+            <ul class="social_links j-c">
+                <li>
+                    <a href="#">
+                        <img src="./images/facebook.png" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="./images/twitter.png" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="./images/youtube.png" alt="">
+                </li>
+                </a>
+            </ul>
+        </div>
+        <div class="header-bottom">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-2 j-c">
+                        <div class="hb-logo">
+                            <img src="./images/logo.png" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-8 j-c-c">
+                        <div class="navigation">
+                            <nav class="navbar navbar-expand-lg navbar-light">
+                                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                                    data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown"
+                                    aria-expanded="false" aria-label="Toggle navigation">
+                                    <span class="navbar-toggler-icon"></span>
+                                </button>
+                                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                                    <ul class="navbar-nav">
+                                        <li class="nav-item active">
+                                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">About</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">Services</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">Career</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">Project</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">contact</a>
+                                        </li>
+
+                                        <li class="nav-item dropdown">
+                                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink"
+                                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                Branch
+                                            </a>
+                                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                                <a class="dropdown-item" href="#">Action</a>
+                                                <a class="dropdown-item" href="#">Another action</a>
+                                                <a class="dropdown-item" href="#">Something else here</a>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 j-c">
+                        <div class="hb_ctc j-c">
+                            <a href="#">
+                                <div class="hbc-left">
+                                    <i class="fas fa-phone-alt"></i>
+                                </div>
+                                <div class="hbc-right">
+                                    <span class="upper_hbc">Need Help? Call Us</span>
+                                    <span class="lower_hbc">(+977)- 01-4486712</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,260 @@
+<?php include 'header.php'?>
+
+<!-- Banner -->
+<section class="main-banner">
+    <div class="container-fluid">
+        <div class="row background">
+            <div class="col-md-6">
+                <div class="mb-content">
+                    <div class="line_wrapper">
+                        <div class="mbc-title">
+                            <span>Since 1989</span>
+                            <h2>Baliyo Contruction</h2>
+                        </div>
+                        <div class="mbc-text">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, provident saepe.
+                                Corrupti
+                                necessitatibu Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum,
+                                provident
+                                saepe. Corrupti
+                                necessitatibu</p>
+                        </div>
+
+                    </div>
+                    <div class="cta">
+                        <a href="#"><i class="fas fa-phone-alt"></i>Call Us</a>
+                        <a href="#"><i class="fas fa-envelope"></i>Email us</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 no-pad">
+                <div class="mb-image">
+                    <img src="./images/banner-image.jpg" alt="">
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ABOUT US -->
+<section class="main-about">
+    <div class="container-fluid">
+        <div class="row max_height">
+            <div class="col-md-6 no-pad">
+                <div class="ma-image">
+                    <img src="./images/about-us.jpg" alt="">
+                </div>
+            </div>
+            <div class="col-md-6 no-pad">
+                <div class="ma-content">
+                    <div class="sec-title">
+                        <img src="./images/Line,png.png" alt=""><span>About Us</span>
+                        <h2>Who are we?</h2>
+                    </div>
+                    <div class="ma-text">
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ipsa provident natus
+                            numquam,
+                            vero placeat! Quasi porro praesentium unde, officiis perferendis veniam aliquid sit,
+                            repellat
+                            illum necessitatibus repudiandae reprehenderit quaerat.
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ipsa provident natus
+                            numquam,
+                            vero placeat!
+                        </p>
+                    </div>
+                    <div class="learn_more">
+                        <a href="#">Learn More</a>
+                    </div>
+                </div>
+                <div class="design_image_wrapper">
+                    <div class="design_image">
+                        <img src="./images/dummny.png" alt="">
+                    </div>
+                    <div class="design_image">
+                        <img src="./images/dummy_0.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- OUR SERVICES -->
+<section class="main-services">
+    <div class="container">
+        <div class="ms-wrapper">
+            <div class="ms-content">
+                <div class="sec-title">
+                    <img src="./images/Line,png.png" alt=""><span>About Us</span>
+                    <h2>Services We Provide</h2>
+                </div>
+                <div class="ma-text width_50">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio eaque soluta quasi molestias
+                        eveniet
+                        excepturi, voluptatum inventore consequatur ipsa, quaerat doloremque quod totam ea vero
+                        asperiores
+                        architecto laudantium possimus enim!</p>
+                </div>
+                <div class="learn_more">
+                    <a href="#">Learn More</a>
+                </div>
+            </div>
+            <div class="services">
+                <div class="row">
+                    <div class="col-md-4 margin-top">
+                        <div class="service-single">
+                            <div class="ms-icon">
+                                <img src="./images/icon_health.png" alt="">
+                                <span>Health & Safety</span>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="service-single">
+                            <div class="ms-icon">
+                                <img src="./images/est.png" alt="">
+                                <span>Health & Safety</span>
+                            </div>
+
+                        </div>
+                        <div class="service-single">
+                            <div class="ms-icon background">
+                                <img src="./images/design.png" alt="">
+                                <span>Health & Safety</span>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-md-4 margin-top_nxt">
+                        <div class="service-single">
+                            <div class="ms-icon background">
+                                <img src="./images/ass.png" alt="">
+                                <span>Health & Safety</span>
+                            </div>
+
+                        </div>
+                        <div class="service-single">
+                            <div class="ms-icon">
+                                <img src="./images/const.png" alt="">
+                                <span>Health & Safety</span>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
+
+<!-- OUR Projects -->
+<section class="main-project mp-margin">
+    <div class="container">
+        <div class="sec-title">
+            <img src="./images/Line,png.png" alt=""><span>About Us</span>
+            <h2>Our Projects</h2>
+        </div>
+        <div class="nav-tab">
+            <nav>
+                <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                    <a class="nav-item nav-link" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab"
+                        aria-controls="nav-home" aria-selected="true">Irrigation</a>
+                    <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab"
+                        aria-controls="nav-profile" aria-selected="false">Road</a>
+                    <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab"
+                        aria-controls="nav-contact" aria-selected="false">Building</a>
+                    <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab"
+                        aria-controls="nav-home" aria-selected="true">Irrigation</a>
+                    <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab"
+                        aria-controls="nav-profile" aria-selected="false">Road</a>
+                    <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab"
+                        aria-controls="nav-contact" aria-selected="false">Building</a>
+                </div>
+            </nav>
+            <div class="tab-content" id="nav-tabContent">
+                <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                    <div class="nav_content_single">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="ncs-single">
+                                    <img src="./images/dummy_0.png" alt="">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="ncs-single">
+                                    <img src="./images/dummny.png" alt="">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="ncs-single">
+                                    <img src="./images/banner-image.jpg" alt="">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="ncs-single">
+                                    <img src="./images/dummy_0.png" alt="">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="ncs-single">
+                                    <img src="./images/dummny.png" alt="">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="ncs-single">
+                                    <img src="./images/banner-image.jpg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                    <div class="nav_content_single">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="ncs-single">
+                                    <img src="./images/dummy_0.png" alt="">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="ncs-single">
+                                    <img src="./images/dummny.png" alt="">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="ncs-single">
+                                    <img src="./images/banner-image.jpg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+                    <div class="nav_content_single">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="ncs-single">
+                                    <img src="./images/dummy_0.png" alt="">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="ncs-single">
+                                    <img src="./images/dummny.png" alt="">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="ncs-single">
+                                    <img src="./images/banner-image.jpg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php include 'footer.php'?>
